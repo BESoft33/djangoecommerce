a6533327713ee7b6fa4d216dbd31d8f07b1af3e6
Santosh Bhattarai <superstargajanikant@gmail.com>
2021-05-10 22:59:01 +0545

    fixed a minor error

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import HomeView
-# from .views import home
+# from .views import HomeView
+from .views import BaseView
 app_name='home'
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('', BaseView.as_view(), name='home'),
     #path('',home,)
 ]
